c7da837cdb384c81c174a60b8a2d791f52b73c51
Karton <rubengm91@hotmail.com>
2018-06-22 11:53:05 +0200

    If you like put checkpoint node, you can with this line (non-proved)

--- Hadoop/Hadoop3.1.0/hadoop_checkpointnode/run.sh
+++ Hadoop/Hadoop3.1.0/hadoop_checkpointnode/run.sh
@@ -4,5 +4,6 @@ chown -R hdmaster:hadoop /var/data/hadoop
 chmod a+rw -R /var/data/hadoop
 chown -R hdmaster:hadoop /var/log/hadoop/
 chmod a+rw -R /var/log/hadoop/
+# sudo -u hdmaster -g hadoop $HADOOP_HOME/bin/hdfs --daemon namenode -checkpoint
 sudo -u hdmaster -g hadoop $HADOOP_HOME/bin/hdfs secondarynamenode
 
